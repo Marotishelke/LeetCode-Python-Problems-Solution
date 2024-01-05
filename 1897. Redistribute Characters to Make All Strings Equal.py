5b32ee64e7285cdd785f873bda352030a086cac4
Marotishelke <71497870+Marotishelke@users.noreply.github.com>
2024-01-05 21:01:46 +0530

    Create 1897. Redistribute Characters to Make All Strings Equal.py

---
+++ 1897. Redistribute Characters to Make All Strings Equal.py
@@ -0,0 +1,19 @@
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        if len(words) == 1:
+            return True
+        
+        total_char_count = sum(len(s) for s in words)
+
+        if total_char_count % len(words) != 0:
+            return False
+
+        my_map = [0] * 26
+        for s in words:
+            for c in s:
+                my_map[ord(c) - ord('a')] += 1
+
+        for i in my_map:
+            if i % len(words) != 0:
+                return False
+        return True
