9d5920d95b1bd0cab13049b54a25b4d1a9065188
Marotishelke <71497870+Marotishelke@users.noreply.github.com>
2024-01-05 21:21:24 +0530

    Array | Sorting

---
+++ 2706. Buy Two Chocolates.py
@@ -0,0 +1,14 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        first_min_cost = second_min_cost = float("inf")
+
+        for p in prices:
+            if p < first_min_cost:
+                second_min_cost, first_min_cost = first_min_cost, p
+            else:
+                second_min_cost = min(second_min_cost, p)
+
+        leftover = money - (first_min_cost + second_min_cost)
+
+        return leftover if leftover >= 0 else money
+                    
