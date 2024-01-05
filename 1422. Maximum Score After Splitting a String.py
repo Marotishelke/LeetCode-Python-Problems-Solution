692b613972d4c809ac419c6f152f88e0b894e1ce
Marotishelke <71497870+Marotishelke@users.noreply.github.com>
2024-01-05 21:24:17 +0530

    String | Prefix Sum

---
+++ 1422. Maximum Score After Splitting a String.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        max_score = count_zeros_left = 0
+        count_ones_right = s.count('1')
+
+        for i in range(len(s) - 1):
+            count_zeros_left += s[i] == '0'
+            count_ones_right -= s[i] == '1'
+            max_score = max(max_score, count_zeros_left + count_ones_right)
+
+        return max_score
+
+        
