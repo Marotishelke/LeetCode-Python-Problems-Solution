30794184504f5289bdc15a2b65da0a5ad3391feb
Maroti Shelke <71497870+Marotishelke@users.noreply.github.com>
2024-02-26 23:27:47 +0530

    Tree | Recursion
    Given the roots of two binary trees p and q, write a function to check if they are the same or not.
    
    Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
    
     
    
    Example 1:
    
    
    Input: p = [1,2,3], q = [1,2,3]
    Output: true
    Example 2:
    
    
    Input: p = [1,2], q = [1,null,2]
    Output: false
    Example 3:
    
    
    Input: p = [1,2,1], q = [1,1,2]
    Output: false

---
+++ 100. Same Tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        #if tree is on same level and same nodes
+        if p == None and  q == None:
+            return True
+        
+        # if level of tree is different 
+        if p == None or q == None:
+            return False
+        
+        # Node value is different
+        if p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
